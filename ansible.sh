1eb7dcc0ff4cae0c5f04b0d4a700910745a4e276
chaitu <chaitus.ch09@gmail.com>
2024-05-06 12:37:57 -0700

    adding elk node creation

--- ansible.sh
+++ ansible.sh
@@ -1,4 +1,4 @@
 ##
-ansible-playbook get-secrets.yml -e vault_token=$vault_token -e env=$env -e role_name=$component
+#ansible-playbook get-secrets.yml -e vault_token=$vault_token -e env=$env -e role_name=$component
 ansible-playbook -i 172.31.90.168, -e ansible_user=ec2-user -e ansible_password=DevOps321 frontend.yml
 #ansible-playbook  expense.yml
